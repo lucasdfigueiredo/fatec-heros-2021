7ac35c4018e159b1e0e0e4c45e3f3e140c08a54c
lucasdfigueiredo <lucas.figueiredo3@fatec.sp.gov.br>
2021-11-25 17:12:27 -0300

    Tutorial 4 final

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from '../mock-heroes';
-import { Hero } from './hero';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,20 +12,34 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
 
-  heroes = HEROES;
+  //heroes = HEROES;
+  heroes: Hero[] = [];
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
-  constructor() { }
+  constructor(private heroService: HeroService, 
+              private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  // getHeroes(): void {
+    // Essa chamada é Bloqueante!!!!
+  //  this.heroes = this.heroService.getHeroes();
+  //}
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(
+          //Esse codigo vai ser execultado em
+          //algum momento (assincrono/ não bloqueante)
+          //
+          // Esse simbolo =>  é uma arrow function
+          heroes => this.heroes = heroes);
+  }
 }
